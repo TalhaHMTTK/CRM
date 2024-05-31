d59b9f74141e0a9cae2505e2d98ad60f65db90ed
TalhaHMTTK <talhahmttk2@gmail.com>
2024-05-31 14:41:52 +0500

    on index show links display all the user record

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,10 @@ class ContactsController < ApplicationController
     def index
         @contacts = Contact.all
     end
+
+
+    def show 
+        em = Contact.find(params[:id])
+        @contact =  Contact.where(:email => em.email)
+    end
 end
